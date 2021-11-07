104415cb97c79ecfd3e0f68958684b6f8f85436b
Kamal Hosen <kamalhosen8920@gmail.com>
2021-11-07 12:35:06 +0600

    inital release

---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Order Test For All for WooCommerce ===
+Contributors: ikamal
+Donate link: https://kamal.pw/
+Tags: wc order test, wc order, woocommerce, woocommerce checkout, woocommerce order, woocommerce orders
+Requires at least: 3.0.1
+Requires PHP: 5.6
+Tested up to: 5.8
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Make sure your checkout works. Test your WooCommerce order process in seconds.
+
+== Description ==
+A payment gateway plugin for WooCommerce to see if your checkout works.
+
+See plugin's GitHub repository [Order Test For All for WooCommerce](https://github.com/ikamal7/wc-order-test-all)
+
+### **Required Plugin**
+* WooCommerce
+
+### **PRIVACY POLICY**
+
+We are not collecting any of your personal data or any kind of logs. So there will be no chance to violate your privacy through this plugin.
+
+### **ABOUT THE MAKER**
+
+I am [Kamal Hosen](https://kamal.pw/), a Bangladeshi full-stack WordPress developer and free and open source enthusiast. As Order Test For All for WooCommerce is an open-source project, you can encourage me by giving me a [star on GitHub](https://github.com/ikamal7/wc-order-test-all). Nothing also, you can contact me on my [LinkedIn](https://www.linkedin.com/in/khosen) for consultation or just to say hello. I love talking to new people. So don’t hesitate.
+
+== Installation ==
+
+= Install from WP Dashboard =
+  * Log into WP dashboard then click **Plugins** > **Add new** > Then under the title "Install Plugins" click **Upload** > **choose the zip** > **Activate the plugin!**
+
+= Install from FTP =
+  * Extract the zip file and drop the contents in the wp-content/plugins/ directory of your WP installation and then activate the Plugin from Plugins page. 
+
+= THAT IS IT: You're done! =
+
+== Frequently Asked Questions ==
+
+= Do I need WooCommerce plugin installed for this to work =
+
+YES.  This sets up a test gateway for WooCommerce only.
+
+== Screenshots ==
+
+1. This is the settings page withing WooCommerce setting area
+2. This Settings for user controls page
+
+== Changelog ==
+
+= 1.0  October 31, 2021  =
+* First release of plugin.
+
+
+== Upgrade Notice ==
+
+
+= 1.0 =
+Stay up to date with this plugins to make sure that you have the latest and greatest version on your site.
---
+++ wc-order-test-all.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Plugin Name: Order Test For All for WooCommerce
+ * Plugin URI:  https://github.com/ikamal7/wc-order-test-all
+ * Description: A payment gateway plugin for WooCommerce to see if your checkout works.
+ * Author:      Kamal Hosen
+ * Author URI:  https://kamal.pw/
+ * Language URI: /languages
+ * text-domain: wc_order_test
+ * Version:     1.0
+ */
+
+function wc_order_test_init() {
+	if (!class_exists('WC_Payment_Gateway')) {
+		return;
+	}
+	
+	class WC_Test_Order_Gateway extends WC_Payment_Gateway {
+	
+		public function __construct() {
+			$this->id = 'wc_order_test';
+			$this->has_fields = false;
+			$this->method_title = __( 'WC Order Test', 'wc_order_test' );
+			$this->init_form_fields();
+			$this->init_settings();
+			$this->title = 'Test Gateway';
+	
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		}
+		
+		function init_form_fields() {
+			$this->form_fields = [
+				'enabled' => [
+					'title' => __( 'Enable/Disable', 'wc_order_test' ),
+					'type' => 'checkbox',
+					'label' => __( 'Enable order test gateway', 'wc_order_test' ),
+					'default' => 'yes'
+				],
+				'enabled_all' => [
+					'title' => __( 'Enable/Disable All Users', 'wc_order_test' ),
+					'type' => 'checkbox',
+					'label' => __( 'Enable gateway for all', 'wc_order_test' ),
+					'default' => 'yes'
+				],
+			];
+		}
+	    
+		
+		public function admin_options() {
+			echo '	<h3>Order Test Gateway</h3><br>
+			<p>Enable this below to test the checkout process on your site.</p>
+				<table class="form-table">';
+				
+			$this->generate_settings_html();
+			
+			echo '	</table>';
+		}
+	
+		public function process_payment( $order_id ) {
+			global $woocommerce;
+	    
+			$order = new WC_Order( $order_id );
+			$order->payment_complete();
+			$order->reduce_order_stock();
+			$woocommerce->cart->empty_cart();
+	
+			return array(
+				'result' => 'success',
+				//'redirect' => add_query_arg('key', $order->order_key, add_query_arg('order', $order->id, get_permalink(woocommerce_get_page_id('thanks')))),
+				'redirect' => $order->get_checkout_order_received_url()
+			);
+		}
+	
+	}	
+
+	function add_user_roles_to_gateway( $methods ) {
+		$enabled_all = get_option( 'woocommerce_wc_order_test_settings');
+		if (current_user_can('administrator') || $enabled_all['enabled_all'] == 'yes' || WP_DEBUG ) {
+			$methods[] = 'WC_Test_Order_Gateway';
+		}
+		
+		return $methods;
+	}
+	
+	add_filter('woocommerce_payment_gateways', 'add_user_roles_to_gateway' );
+	
+}
+
+add_filter('plugins_loaded', 'wc_order_test_init' );
+
+?>
